4622a7849d94ab7965ebdbe35a0d1edaaa18ce74
Fahri Can <fahri-can@gmx.at>
2023-12-16 23:15:53 +0100

    chore: Adapt UI by using more components.

---
+++ src/components/TaskDetailCardComponent.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+
+import {PropType} from "vue";
+import {TaskFetchResponse} from "@/types/TaskFetchResponse";
+
+const props = defineProps({
+  task: Object as PropType<TaskFetchResponse>
+});
+
+const formattedDate = (date: string) => {
+  const options = {year: 'numeric', month: '2-digit', day: '2-digit'};
+  return new Date(date).toLocaleDateString('en-US', options).replace(/\//g, '.');
+}
+
+</script>
+
+<template>
+  <v-card class="mx-auto center-card-text">
+    <v-list lines="two">
+      <v-list-subheader>Task Detail</v-list-subheader>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Task ID: </span> {{ props.task.id }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Description: </span> {{ props.task.description }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Reminder set: </span> {{ props.task.isReminderSet }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Task open: </span> {{ props.task.isTaskOpen }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Created on: </span> {{ formattedDate(props.task.createdOn) }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <template v-slot:subtitle>
+          <span class="font-weight-bold">Priority: </span> {{ props.task.priority }}
+        </template>
+      </v-list-item>
+      <v-divider inset></v-divider>
+
+    </v-list>
+  </v-card>
+
+</template>
+
+<style scoped>
+
+.center-card-text {
+  text-align: center;
+}
+
+</style>
--- src/views/TaskDetailView.vue
+++ src/views/TaskDetailView.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import {TaskFetchResponse} from "@/types/TaskFetchResponse";
 import {taskService} from "@/services/TaskApi";
+import {TaskState} from "@/types/TaskState";
+import {ALL_TASKS, CLOSED_TASKS, OPEN_TASKS} from "@/constants/constants";
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import TaskDetailCardComponent from "@/components/TaskDetailCardComponent.vue";
+import AppBackgroundComponent from "@/components/AppBackgroundComponent.vue";
 
 
 const props = defineProps({
   id: Number
 });
+
+const selectedTaskType = ref(TaskState[TaskState.OPEN]);
 
 const task = reactive<TaskFetchResponse>({
   id: 0,
@@ -18,6 +25,20 @@ const task = reactive<TaskFetchResponse>({
   priority: null
 })
 
+const handleTaskTypeSelected = (taskType: string) => {
+  switch (taskType) {
+    case OPEN_TASKS:
+      selectedTaskType.value = TaskState[TaskState.OPEN];
+      break;
+    case CLOSED_TASKS:
+      selectedTaskType.value = TaskState[TaskState.CLOSED];
+      break;
+    case ALL_TASKS:
+      selectedTaskType.value = '';
+      break;
+  }
+};
+
 async function fetchTaskById() {
   try {
     const response = await taskService.getTaskBydId(props.id);
@@ -27,71 +48,14 @@ async function fetchTaskById() {
   }
 }
 
-const formatedDate = (date: string) => {
-  const options = {year: 'numeric', month: '2-digit', day: '2-digit'};
-  return new Date(date).toLocaleDateString('en-US', options).replace(/\//g, '.');
-}
-
 fetchTaskById()
 </script>
 
 <template>
 
-  <v-card class="mx-auto center-card-text">
-    <v-list lines="two">
-      <v-list-subheader>Task Detail</v-list-subheader>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Task ID: </span> {{ task.id }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Description: </span> {{ task.description }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Reminder set: </span> {{ task.isReminderSet }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Task open: </span> {{ task.isTaskOpen }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Created on: </span> {{ formatedDate(task.createdOn) }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Priority: </span> {{ task.priority }}
-        </template>
-      </v-list-item>
-      <v-divider inset></v-divider>
-
-    </v-list>
-  </v-card>
+  <NavbarComponent @taskTypeSelected="handleTaskTypeSelected"/>
+  <AppBackgroundComponent>
+    <TaskDetailCardComponent :task="task"/>
+  </AppBackgroundComponent>
 
 </template>
-
-<style scoped>
-
-.center-card-text {
-  text-align: center;
-}
-
-</style>
