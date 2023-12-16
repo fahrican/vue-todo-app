1a7527a788b553418ae7f0a0f354ccd77fa598cc
Fahri Can <fahri-can@gmx.at>
2023-12-16 22:46:02 +0100

    chore: Extract component logic.

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,32 +1,17 @@
 <template>
   <NavbarComponent @taskTypeSelected="handleTaskTypeSelected"/>
 
-  <v-app id="inspire">
-    <v-main class="bg-grey-lighten-3">
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-              class="v-sheet-padding"
-            >
-              <CardComponent
-                v-if="!showTaskDetails"
-                :tasks="tasks"
-                @card-clicked="handleCardClicked"
-              />
-              <TaskDetailComponent
-                v-if="showTaskDetails"
-                :taskId="selectedTaskId"
-              />
-
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <AppBackgroundComponent>
+    <CardComponent
+      v-if="!showTaskDetails"
+      :tasks="tasks"
+      @card-clicked="handleCardClicked"
+    />
+    <TaskDetailComponent
+      v-if="showTaskDetails"
+      :taskId="selectedTaskId"
+    />
+  </AppBackgroundComponent>
 
 </template>
 
@@ -39,6 +24,7 @@ import {TaskState} from "@/types/TaskState";
 import {taskService} from "@/services/TaskApi";
 import {ALL_TASKS, CLOSED_TASKS, OPEN_TASKS} from "@/constants/constants";
 import TaskDetailComponent from "@/components/TaskDetailComponent.vue";
+import AppBackgroundComponent from "@/components/AppBackgroundComponent.vue";
 
 
 const tasks = reactive<TaskFetchResponse[]>([])
