5ad59d0ab9b8c546422d1d9a929a98d2da0733b2
Fahri Can <fahri-can@gmx.at>
2023-12-19 21:09:16 +0100

    chore: Create form to create a new task.

--- src/components/TaskCreateComponent.vue
+++ src/components/TaskCreateComponent.vue
@@ -1,47 +1,31 @@
 <script setup lang="ts">
 import {ref} from 'vue'
 import {useField, useForm} from 'vee-validate'
+import {Priority} from "@/types/Priority";
 
 const {handleSubmit, handleReset} = useForm({
   validationSchema: {
-    name(value) {
-      if (value?.length >= 2) return true
+    description(value) {
+      if (value?.length >= 3) return true
 
-      return 'Name needs to be at least 2 characters.'
-    },
-    phone(value) {
-      if (value?.length > 9 && /[0-9-]+/.test(value)) return true
-
-      return 'Phone number needs to be at least 9 digits.'
-    },
-    email(value) {
-      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
-
-      return 'Must be a valid e-mail.'
+      return 'Description needs to be at least 3 characters.'
     },
     select(value) {
       if (value) return true
 
-      return 'Select an item.'
-    },
-    checkbox(value) {
-      if (value === '1') return true
-
-      return 'Must be checked.'
+      return 'Select a priority!.'
     },
   },
 })
-const name = useField('name')
-const phone = useField('phone')
-const email = useField('email')
+const description = useField('description')
 const select = useField('select')
-const checkbox = useField('checkbox')
+const isReminderSet = useField('isReminderSet')
+const isTaskOpen = useField('isTaskOpen')
 
-const items = ref([
-  'Item 1',
-  'Item 2',
-  'Item 3',
-  'Item 4',
+const priority = ref([
+  Priority[Priority.LOW],
+  Priority[Priority.MEDIUM],
+  Priority[Priority.HIGH],
 ])
 
 const submit = handleSubmit(values => {
@@ -52,53 +36,56 @@ const submit = handleSubmit(values => {
 <template>
   <form @submit.prevent="submit">
     <v-text-field
-      v-model="name.value.value"
+      v-model="description.value.value"
       :counter="10"
-      :error-messages="name.errorMessage.value"
-      label="Name"
+      :error-messages="description.errorMessage.value"
+      label="Description"
     ></v-text-field>
-
-    <v-text-field
-      v-model="phone.value.value"
-      :counter="7"
-      :error-messages="phone.errorMessage.value"
-      label="Phone Number"
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email.value.value"
-      :error-messages="email.errorMessage.value"
-      label="E-mail"
-    ></v-text-field>
-
-    <v-select
-      v-model="select.value.value"
-      :items="items"
-      :error-messages="select.errorMessage.value"
-      label="Select"
-    ></v-select>
 
     <v-checkbox
-      v-model="checkbox.value.value"
-      :error-messages="checkbox.errorMessage.value"
+      v-model="isReminderSet.value.value"
+      :error-messages="isReminderSet.errorMessage.value"
       value="1"
-      label="Option"
+      label="Reminder"
       type="checkbox"
     ></v-checkbox>
 
-    <v-btn
-      class="me-4"
-      type="submit"
-    >
-      submit
-    </v-btn>
+    <v-checkbox
+      v-model="isTaskOpen.value.value"
+      :error-messages="isTaskOpen.errorMessage.value"
+      value="1"
+      label="Reminder"
+      type="checkbox"
+    ></v-checkbox>
 
-    <v-btn @click="handleReset">
-      clear
-    </v-btn>
+    <v-select
+      v-model="select.value.value"
+      :items="priority"
+      :error-messages="select.errorMessage.value"
+      label="Priority"
+    ></v-select>
+
+    <v-btn class="mb-4 clear-btn" @click="handleReset">clear</v-btn>
+
+    <v-btn class="mb-4 submit-btn" type="submit">submit</v-btn>
+
   </form>
 </template>
 
 <style scoped>
+
+.v-btn.clear-btn {
+  width: 49%;
+  background-color: red;
+  color: #EEEEEE;
+  margin: 0 0.2rem;
+}
+
+.v-btn.submit-btn {
+  width: 49%;
+  background-color: green;
+  color: #EEEEEE;
+  margin: 0 0.2rem;
+}
 
 </style>
