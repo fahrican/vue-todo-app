813dbe749c9af0d33a146954860d3565ccc7e11c
Fahri Can <fahri-can@gmx.at>
2023-12-23 23:01:14 +0100

    chore: Enable updating task.

--- src/components/TaskUpdateComponent.vue
+++ src/components/TaskUpdateComponent.vue
@@ -1,84 +1,70 @@
 <script setup lang="ts">
 
 import {reactive, ref} from "vue";
-import {useField, useForm} from "vee-validate";
-import {Priority} from "@/types/Priority";
 import {TaskUpdateRequest} from "@/types/TaskUpdateRequest";
 import {useTaskStore} from "@/store/taskStore";
+import {Priority} from "@/types/Priority";
+import {useField} from "vee-validate";
 
 
-const {handleSubmit, handleReset} = useForm({
-  validationSchema: {
-    description(value) {
-      if (value?.length >= 3) return true
-      return 'Description needs to be at least 3 characters.'
-    },
-    select(value) {
-      if (value) return true
-      return 'Select a priority!.'
-    },
-  },
-})
-const description = useField('description');
-const select = useField('select');
-const isReminderSet = useField('isReminderSet');
-const isTaskOpen = useField('isTaskOpen');
 const priority = ref([
   Priority[Priority.LOW],
   Priority[Priority.MEDIUM],
   Priority[Priority.HIGH],
 ]);
-const request = reactive<TaskUpdateRequest>({
-  description: description.value.value,
-  priority: select.value.value,
-  isReminderSet: isReminderSet.value.value,
-  isTaskOpen: isTaskOpen.value.value,
-});
-const emit = defineEmits(['updated-task']);
 const taskStore = useTaskStore();
+const request = reactive<TaskUpdateRequest>({
+  description: taskStore.taskToEdit.description,
+  isReminderSet: taskStore.taskToEdit.isReminderSet,
+  isTaskOpen: taskStore.taskToEdit.isTaskOpen,
+  priority: taskStore.taskToEdit.priority,
+});
+const description = useField('description');
+const emit = defineEmits(['updated-task', 'abort-clicked']);
 
 
-const submit = handleSubmit(values => {
-  request.description = values.description;
-  request.isReminderSet = values.isReminderSet !== undefined;
-  request.isTaskOpen = values.isTaskOpen !== undefined;
-  request.priority = values.select;
-  emit('updated-task', request);
-})
+const handleSubmit = () => {
+  if (request.description.length >= 3) {
+    emit('updated-task', taskStore.taskToEdit.id, request);
+  } else {
+    description.errorMessage.value = 'Description needs to be at least 3 characters.';
+  }
+};
+
+const handleReset = () => {
+  emit('abort-clicked');
+};
 
 </script>
 
 <template>
-  <form @submit.prevent="submit">
+  <form @submit.prevent="handleSubmit">
     <v-text-field
-      v-model="taskStore.taskToEdit.description"
+      v-model="request.description"
       :counter="10"
       :error-messages="description.errorMessage.value"
       label="Description"
     />
 
     <v-checkbox
-      v-model="taskStore.taskToEdit.isReminderSet"
-      :error-messages="isReminderSet.errorMessage.value"
+      v-model="request.isReminderSet"
       label="Reminder"
       type="checkbox"
     ></v-checkbox>
 
     <v-checkbox
-      v-model="taskStore.taskToEdit.isTaskOpen"
-      :error-messages="isTaskOpen.errorMessage.value"
+      v-model="request.isTaskOpen"
       label="Open"
       type="checkbox"
     ></v-checkbox>
 
     <v-select
-      v-model="taskStore.taskToEdit.priority"
+      v-model="request.priority"
       :items="priority"
-      :error-messages="select.errorMessage.value"
       label="Priority"
     ></v-select>
 
-    <v-btn class="mb-4 clear-btn" @click="handleReset">clear</v-btn>
+    <v-btn class="mb-4 clear-btn" @click="handleReset">abort</v-btn>
 
     <v-btn class="mb-4 submit-btn" type="submit">update</v-btn>
 
--- src/views/TaskUpdateView.vue
+++ src/views/TaskUpdateView.vue
@@ -4,15 +4,33 @@ import AppBackgroundComponent from "@/components/AppBackgroundComponent.vue";
 import NavbarComponent from "@/components/NavbarComponent.vue";
 import {useTaskNavigation} from "@/composables/useTaskNavigation";
 import TaskUpdateComponent from "@/components/TaskUpdateComponent.vue";
+import {TaskUpdateRequest} from "@/types/TaskUpdateRequest";
+import {taskService} from "@/services/TaskApi";
+import router from "@/router";
 
-const {handleTaskTypeSelected, logoClicked} = useTaskNavigation();
+const {handleTaskTypeSelected, logoClicked, navigateToTasksView} = useTaskNavigation();
 
+const clickedAbort = () => {
+  router.back();
+};
 
+const editedTask = (id: number, request: TaskUpdateRequest) => {
+  updateTask(id, request);
+  navigateToTasksView();
+};
+
+async function updateTask(id: number, request: TaskUpdateRequest) {
+  try {
+    await taskService.updateTask(id, request);
+  } catch (err) {
+    console.log('error loading tasks: ' + err)
+  }
+}
 </script>
 
 <template>
   <NavbarComponent @taskTypeSelected="handleTaskTypeSelected" @logoClicked="logoClicked"/>
   <AppBackgroundComponent>
-    <TaskUpdateComponent />
+    <TaskUpdateComponent @updated-task="editedTask" @abort-clicked="clickedAbort"/>
   </AppBackgroundComponent>
 </template>
