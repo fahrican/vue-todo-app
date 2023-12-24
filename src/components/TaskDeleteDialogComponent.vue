889ac45ce3e6332045a92fecc3b2a789366d5656
Fahri Can <fahri-can@gmx.at>
2023-12-24 21:59:07 +0100

    chore: Adjust component.

--- src/components/TaskDeleteDialogComponent.vue
+++ src/components/TaskDeleteDialogComponent.vue
@@ -18,15 +18,12 @@ const confirmDelete = () => {
   dialog.value = false;
   emits('update:modelValue', false);
 };
+
 </script>
 
 <template>
   <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      width="auto"
-    >
+    <v-dialog v-model="dialog" persistent width="auto">
       <v-card>
         <v-card-title class="text-h5">
           Are you sure you want to delete this task?
@@ -37,10 +34,10 @@ const confirmDelete = () => {
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="red-darken-1" variant="text" @click="dialog = false; emits('update:modelValue', false);">
-            Disagree
+            Cancel
           </v-btn>
           <v-btn color="green-darken-1" variant="text" @click="confirmDelete">
-            Agree
+            Confirm
           </v-btn>
         </v-card-actions>
       </v-card>
