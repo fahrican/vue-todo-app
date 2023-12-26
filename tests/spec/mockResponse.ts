1a6241581d298ca2adb37e5b021ee90b8fdc9fe0
Fahri Can <fahri-can@gmx.at>
2023-12-26 23:43:53 +0100

    chore: Add dummy test.

---
+++ tests/spec/mockResponse.ts
@@ -0,0 +1,15 @@
+export const mockTasksResponse = [{
+  "id": 23,
+  "description": "test new nav",
+  "isReminderSet": false,
+  "isTaskOpen": false,
+  "createdOn": "2023-12-26T17:53:53.045334",
+  "priority": "LOW"
+}, {
+  "id": 25,
+  "description": "Write new Unit Tests",
+  "isReminderSet": true,
+  "isTaskOpen": true,
+  "createdOn": "2023-12-26T17:55:41.73961",
+  "priority": "HIGH"
+}]
