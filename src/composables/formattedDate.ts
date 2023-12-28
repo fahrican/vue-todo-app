d6a34519e52a381104d8e6e9d1121a1ed71d7f53
Fahri Can <fahri-can@gmx.at>
2023-12-28 22:59:01 +0100

    chore: Adapt format date logic.

--- src/composables/formattedDate.ts
+++ src/composables/formattedDate.ts
@@ -1,4 +1,21 @@
-export const formattedDate = (date: string) => {
-  const options = {year: 'numeric', month: '2-digit', day: '2-digit'};
-  return new Date(date).toLocaleDateString('en-US', options).replace(/\//g, '.');
-}
+/**
+ * Formats a date string to a specified locale and format.
+ *
+ * @param {string} date - The date string to format.
+ * @param {string} [locale='en-US'] - The locale to use for formatting.
+ * @param {Intl.DateTimeFormatOptions} [options] - The options for date format.
+ * @returns {string} Formatted date string.
+ */
+export const formattedDate = (
+  date: string,
+  locale: string = 'en-US',
+  options: Intl.DateTimeFormatOptions = {year: 'numeric', month: '2-digit', day: '2-digit'}
+): string => {
+  const parsedDate = new Date(date);
+  if (isNaN(parsedDate.getTime())) {
+    // Handle invalid date
+    console.warn('formattedDate: Invalid date provided');
+    return '';
+  }
+  return parsedDate.toLocaleDateString(locale, options).replace(/\//g, '.');
+};
--- tests/spec/formattedDate.test.ts
+++ tests/spec/formattedDate.test.ts
@@ -1,15 +1,24 @@
-import { test, expect } from 'vitest'
+import {expect, describe, it} from 'vitest'
 import {formattedDate} from "../../src/composables/formattedDate";
 
-test('formattedDate formats date correctly', () => {
-  const date1 = '2022-12-31';
-  const expected1 = '12.31.2022';
-  const result1 = formattedDate(date1)
 
-  const date2 = '2024-01-01'
-  const expected2 = '01.01.2024';
-  const result2 = formattedDate(date2);
+describe('formattedDate Unit Tests', () => {
+  it('formattedDate formats date correctly', () => {
+    const date1 = '2022-12-31';
+    const expected1 = '12.31.2022';
+    const result1 = formattedDate(date1)
 
-  expect(result1).toBe(expected1);
-  expect(result2).toBe(expected2);
-})
+    const date2 = '2024-01-01'
+    const expected2 = '01.01.2024';
+    const result2 = formattedDate(date2);
+
+    expect(result1).toBe(expected1);
+    expect(result2).toBe(expected2);
+  });
+
+  it('formattedDate returns empty string for invalid date', () => {
+    const invalidDate = 'invalid-date';
+    const result = formattedDate(invalidDate);
+    expect(result).toBe('');
+  })
+});
