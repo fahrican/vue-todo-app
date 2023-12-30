feb322287a09f5a91abb2b46b3d76d69397f682b
Fahri Can <fahri-can@gmx.at>
2023-12-30 02:23:08 +0100

    chore: Adapt create task method.

--- src/views/TaskCreateView.vue
+++ src/views/TaskCreateView.vue
@@ -7,28 +7,35 @@ import {useTaskNavigation} from '@/composables/useTaskNavigation';
 import {TaskCreateRequest} from "@/types/taskDto";
 import {ref} from "vue";
 import SpinningLoadingComponent from "@/components/SpinningLoadingComponent.vue";
+import {AxiosError} from "axios";
+import logRequestError from "@/composables/logRequestError";
+import ErrorDialogComponent from "@/components/ErrorDialogComponent.vue";
+
 
 const {handleTaskTypeSelected, logoClicked, navigateToTasksView} = useTaskNavigation();
 const isLoading = ref(false);
+const isNetworkError = ref(false);
+const axiosError = ref<AxiosError>();
 
-const createTask = (task: TaskCreateRequest) => {
-  createNewTask(task);
-  navigateToTasksView();
-};
 
-async function createNewTask(request: TaskCreateRequest): Promise<void> {
+async function createTask(request: TaskCreateRequest): Promise<void> {
   isLoading.value = true;
   await taskService.createTask(request)
     .then(() => {
       isLoading.value = false;
+      isNetworkError.value = false;
+      navigateToTasksView();
     })
-    .catch((err) => {
-      console.log('error creating task: ' + err);
+    .catch((err: AxiosError) => {
+      logRequestError('createNewTask', err);
+      axiosError.value = err instanceof AxiosError ? err : undefined;
+      isNetworkError.value = true;
     })
     .finally(() => {
       isLoading.value = false;
     });
 }
+
 </script>
 
 <template>
@@ -36,6 +43,7 @@ async function createNewTask(request: TaskCreateRequest): Promise<void> {
   <AppBackgroundComponent>
     <TaskCreateComponent @create-new-task="createTask"/>
     <SpinningLoadingComponent :is-loading="isLoading"/>
+    <ErrorDialogComponent :model-value="isNetworkError" :axios-error="axiosError"/>
   </AppBackgroundComponent>
 </template>
 
