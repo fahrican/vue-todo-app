5611ac232607aae0b56631128e9f004e1ea04769
Fahri Can <fahri-can@gmx.at>
2023-12-31 23:20:08 +0100

    chore: Extract create task logic.

--- src/views/TaskCreateView.vue
+++ src/views/TaskCreateView.vue
@@ -2,14 +2,13 @@
 import AppBackgroundComponent from "@/components/AppBackgroundComponent.vue";
 import NavbarComponent from "@/components/NavbarComponent.vue";
 import TaskCreateComponent from "@/components/TaskCreateComponent.vue";
-import {taskService} from "@/services/taskApi";
 import {useTaskNavigation} from '@/composables/useTaskNavigation';
 import {TaskCreateRequest} from "@/types/taskDto";
 import {ref} from "vue";
 import SpinningLoadingComponent from "@/components/SpinningLoadingComponent.vue";
 import {AxiosError} from "axios";
-import logRequestError from "@/composables/logRequestError";
 import ErrorDialogComponent from "@/components/ErrorDialogComponent.vue";
+import {generateTask} from "@/composables/generateTask";
 
 
 const {handleTaskTypeSelected, logoClicked, navigateToTasksView} = useTaskNavigation();
@@ -18,22 +17,8 @@ const isNetworkError = ref(false);
 const axiosError = ref<AxiosError>();
 
 
-async function createTask(request: TaskCreateRequest): Promise<void> {
-  isLoading.value = true;
-  isNetworkError.value = false;
-  await taskService.createTask(request)
-    .then(() => {
-      isLoading.value = false;
-      navigateToTasksView();
-    })
-    .catch((err: AxiosError | unknown) => {
-      logRequestError('createNewTask', err);
-      axiosError.value = err instanceof AxiosError ? err : undefined;
-      isNetworkError.value = true;
-    })
-    .finally(() => {
-      isLoading.value = false;
-    });
+const createTask = (request: TaskCreateRequest) => {
+  generateTask(request, isLoading, isNetworkError, axiosError, navigateToTasksView);
 }
 
 </script>
